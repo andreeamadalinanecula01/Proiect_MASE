7b9cc787bd538e4580cf9faa357cbf211927752c
Madalina Andreea Necula <madalinaandreeanecula@Madalinas-MacBook-Air.local>
2025-05-21 21:11:05 +0300

    Ștergere fișiere Render

--- eenvironment/manage.py
+++ eenvironment/manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eenvironment.settings')
+    #Pentru rulare locala 
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eenvironment.settings')
+    #Pentru production
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eenvironment.settings_production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
